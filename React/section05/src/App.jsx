a08036152d02d8c9050b82015cda539c58f7a1ab
FreshSangchoo <aprldpdy289@naver.com>
2025-07-04 20:53:30 +0900

    250704 Study React Infrun ~54

--- React/section05/src/App.jsx
+++ React/section05/src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import HookExam from "./components/HookExam";
+import Register from "./components/Register";
+
+// React의 컴포넌트가 리렌더링 되는 경우
+// 1. 자신이 관리하는 State의 값이 변경된 경우
+// 2. 자신이 제공받는 props의 값이 변경된 경우
+// 3. 부모 컴포넌트가 리렌더링되는 경우
 
 // 컴포넌트의 첫 글자는 대문자
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <HookExam />
     </>
   );
 }
