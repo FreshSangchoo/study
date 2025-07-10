d5529bf32c062712805ff8d308e5b1dcd28b9307
FreshSangchoo <aprldpdy289@naver.com>
2025-07-10 21:31:00 +0900

    250710 Study React Infrun ~87

---
+++ React/section12/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header
+        title={"2025년 7월"}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+      <DiaryList />
+    </div>
+  );
+};
+
+export default Home;
